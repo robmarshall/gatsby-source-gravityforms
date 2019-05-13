1c45f990e6b700d6ad929253f3eb1eb2e3600c9e
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-05-13 22:29:47 +0100

    Working on bringing across settingss

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,12 +1,12 @@
-exports.sourceNodes = async ({ actions }) => {
+const axios = require('axios')
+
+exports.sourceNodes = async (
+    { boundActionCreators: { createNode }, createNodeId },
+    { plugins, baseUrl, api, basicAuth }
+) => {
     const { createNode } = actions
-    // Create nodes here, generally by downloading data
-    // from a remote API.
-    const data = await fetch(REMOTE_API)
 
-    // Process data into nodes.
-    data.forEach(datum => createNode(processDatum(datum)))
-
-    // We're done, return.
-    return
+    console.log(baseUrl)
+    console.log(api)
+    console.log(basicAuth)
 }
