62636921a8483442fbd255a6d87313c29f3c783d
Rob Marshall <rob@fox.agency>
2019-05-22 19:08:16 +0100

    Working on processing form api data

--- src/utils/processForms.js
+++ src/utils/processForms.js
@@ -1,35 +1,52 @@
 const { isArray, isObject } = require('./helpers')
 
 const processForms = (createContentDigest, formObj) => {
-    let formFields = {
+    let newFormObj = {
         internal: {
             contentDigest: createContentDigest(formObj),
             type: 'GF__Form',
         },
     }
 
-    for (const [key, value] of Object.entries(formObj)) {
-        // Check if value is an array
-        if (!isArray(formObj[key])) {
-            formFields[key] = formObj[key]
+    // Set fields to skip. This could be due to sensitive info
+    // or not needed on the frontend
+    const ignoreFields = ['notifications']
 
-            // Check if value is an object
-            if (!isObject(formObj[key])) {
-                //console.log('Key: ' + key)
-                //console.log('Value: ' + formObj[key])
+    for (const [key, value] of Object.entries(formObj)) {
+        if (!ignoreFields.includes(key)) {
+            // Check if value is an array
+            if (!isArray(formObj[key])) {
+                newFormObj[key] = formObj[key]
             } else {
-                //console.log(key + ' is an object!')
-                // Objects do not need to be ordered in the node
+                let newContent = {}
+                let arrayCount = 0
+
+                formObj[key].forEach(arr => {
+                    // Today arrays do not start at 0
+                    // Because we are turning these to objects
+                    arrayCount = arrayCount + 1
+                    newContent[arrayCount] = arr
+                })
+
+                // Gatsby has saved 'fields' for its own use
+                // We need to rename Gravity Form fields to something new
+                if (key == 'fields') {
+                    newFormObj['formFields'] = newContent
+
+                    // @TODO
+                    // Need to create processor that fixes
+                    // type issues
+                    // and opens up choices into objects
+                } else {
+                    newFormObj[key] = newContent
+                }
             }
-        } else {
-            //console.log(key + ' is an array!')
-            // Array elements need to be ordered, or given an order key in the node
         }
     }
 
-    formFields.id = `gravity-form-${formFields.id}`
+    newFormObj.id = `gravity-form-${newFormObj.id}`
 
-    return formFields
+    return newFormObj
 }
 
 module.exports = {
