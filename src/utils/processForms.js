72e6f464763d87bdca3e013ec611edbc75838efa
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-05-25 11:50:08 +0100

    Added function description

--- src/utils/processForms.js
+++ src/utils/processForms.js
@@ -1,6 +1,20 @@
 const { isArray, isObject } = require('./helpers')
 
-const processForms = (createContentDigest, formObj) => {
+/**
+ * Take raw Gravity Forms data and turn into usable
+ * Gatsby consumable objects.
+ * @param  function     createContentDigest Gatsby helper function
+ * @param  object       formObj             Raw Gravity Forms form info
+ * @param  array        ignoreFields        Array of top level fields to ignore
+ * @return object                           Processed form data
+ */
+
+const processForms = (createContentDigest, formObj, ignoreFields) => {
+    // Add the Gatsby internal information.
+    // contentDigest tracks any changes and lets Gatsby know if
+    // the data needs to be rerendered, or is everything is
+    // the same.
+
     let newFormObj = {
         internal: {
             contentDigest: createContentDigest(formObj),
@@ -8,13 +22,29 @@ const processForms = (createContentDigest, formObj) => {
         },
     }
 
-    // Set fields to skip. This could be due to sensitive info
-    // or not needed on the frontend
-    const ignoreFields = ['notifications']
+    // Ignore fields sets fields to skip. This could be due to
+    // sensitive info or not needed on the frontend
+    // We need to ensure the ignore fields are only an array.
+    // If it is anything else, it will break the loop. If it
+    // is not an array, empty!
+
+    // TODO: Use an object instead of an array. But array was
+    // easier for now.
+
+    ignoreFields = isArray(ignoreFields) ? ignoreFields : null
+
+    // Loop through the form fields. Check we want to show all
+    // fields, ensure fields are formatted correctly,
+    // updated fields that are called protected names (e.g. fields)
+    // Finally, add a Gatsby ID
 
     for (const [key, value] of Object.entries(formObj)) {
         if (!ignoreFields.includes(key)) {
-            // Check if value is an array
+            // If the value is an object, add it right away.
+            // No changes are needed (yet).
+            //
+            // TODO: Check if there are arrays within the object
+
             if (!isArray(formObj[key])) {
                 newFormObj[key] = formObj[key]
             } else {
