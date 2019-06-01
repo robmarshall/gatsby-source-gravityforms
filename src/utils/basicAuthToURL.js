caee2f6a72ca2d8b87eb56c5e5fce289f78656ad
DESKTOP-U1LMLG9\Rob Laptop <robertmarshallproductions@gmail.com>
2019-06-01 12:21:31 +0100

    Fixed if statement logic

--- src/utils/basicAuthToURL.js
+++ src/utils/basicAuthToURL.js
@@ -4,23 +4,25 @@ function addBasicAuthToURL(basicAuth, url) {
     // Check basic auth
     if (!basicAuth.username || !basicAuth.password) {
         console.log('Basic auth details not included')
-    }
-
-    // Check protocol exists
-    if (protocol.includes('http')) {
-        // Re-add // back to string. This is needed later for replacement
-        protocol = protocol + '//'
-
-        // Add basic auth after protocol
-        const authString =
-            protocol + basicAuth.username + ':' + basicAuth.password + '@'
-
-        // Replace current protocol with new string
-        const newUrl = url.replace(protocol, authString)
-        return newUrl
     } else {
-        console.log('Protocol not included.')
+        // Check protocol exists
+        if (protocol.includes('http')) {
+            // Re-add // back to string. This is needed later for replacement
+            protocol = protocol + '//'
+
+            // Add basic auth after protocol
+            const authString =
+                protocol + basicAuth.username + ':' + basicAuth.password + '@'
+
+            // Replace current protocol with new string
+            const newUrl = url.replace(protocol, authString)
+            return newUrl
+        } else {
+            console.log('Protocol not included.')
+        }
     }
+
+    // Return unchanged URL
     return url
 }
 
