d3770d75b1d5fffd02497c0542f7449292333d5d
robmarshall <robertmarshllproductions@gmail.com>
2019-08-03 10:17:53 +0100

    Added info on incorrect URL struct

--- src/utils/basicAuthToURL.js
+++ src/utils/basicAuthToURL.js
@@ -1,25 +1,37 @@
 function addBasicAuthToURL(basicAuth, url) {
-    let protocol = url.split('//')[0]
+    // Check the URL is set up correctly
+    if (url.includes('//')) {
+        let protocol = url.split('//')[0]
 
-    // Check basic auth
-    if (!basicAuth.username || !basicAuth.password) {
-        console.log('Basic auth details not included')
-    } else {
-        // Check protocol exists
-        if (protocol.includes('http')) {
-            // Re-add // back to string. This is needed later for replacement
-            protocol = protocol + '//'
-
-            // Add basic auth after protocol
-            const authString =
-                protocol + basicAuth.username + ':' + basicAuth.password + '@'
-
-            // Replace current protocol with new string
-            const newUrl = url.replace(protocol, authString)
-            return newUrl
+        // Check basic auth
+        if (!basicAuth.username || !basicAuth.password) {
+            console.log('Basic auth details not included')
         } else {
-            console.log('Protocol not included.')
+            // Check protocol exists
+            if (protocol.includes('http')) {
+                // Re-add // back to string. This is needed later for replacement
+                protocol = protocol + '//'
+
+                // Add basic auth after protocol
+                const authString =
+                    protocol +
+                    basicAuth.username +
+                    ':' +
+                    basicAuth.password +
+                    '@'
+
+                // Replace current protocol with new string
+                const newUrl = url.replace(protocol, authString)
+                return newUrl
+            } else {
+                console.log('Protocol not included.')
+            }
         }
+    } else {
+        console.log(
+            'URL structure is different to recommended. This may cause issues.'
+        )
+        console.log('Try "https://[yoursitename].com" - or equivalent')
     }
 
     // Return unchanged URL
