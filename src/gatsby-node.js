100b12b4077fc4a78978a60fd84a73c676659cec
Alex Moon <moonmeister@users.noreply.github.com>
2020-12-03 04:16:06 -0600

    Cleanup logging (#22)
    * feat: add config option to allow unsigned TLS in gatsby-config and silence node warning
    
    * chore: move off of chalk to gatsby's internal reporter for cleaner logs, verbose support, and panics on fatal errors
    
    * fix: add default value for allowSelfSigned
    
    * fix: typos

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,26 +1,17 @@
-const chalk = require('chalk')
-
 const { getFormsAndFields } = require('./utils/axios')
 const { processForms } = require('./utils/processForms')
-
-const log = console.log
 
 let activeEnv =
     process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
 
-// If we are in dev, ignore the fact that we are using a fake SSL certificate
-if (activeEnv == 'development') {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-}
-
 exports.sourceNodes = async (
-    { actions: { createNode }, createContentDigest, createNodeId },
+    { actions: { createNode }, createContentDigest, createNodeId, reporter },
     {
-        plugins,
         baseUrl,
         api,
         include,
         exclude,
+        allowSelfSigned = false,
         basicAuth = {
             username: '',
             password: '',
@@ -28,20 +19,28 @@ exports.sourceNodes = async (
         ignoreFields = ['notifications'],
     }
 ) => {
-    log(chalk.black.bgWhite('Starting Gravity Forms Source plugin'))
+    global.reporter = reporter;
+    // If we are in dev, ignore the fact that we are using a fake SSL certificate
+    if (activeEnv == 'development' && allowSelfSigned) {
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+    }
+
+    reporter.verbose('Starting Gravity Forms Source plugin')
 
     // Run initial checks
     if (!baseUrl) {
-        log(chalk.bgRed('We could not find a baseUrl in your gatsby-config.js'))
+        reporter.panic(
+            'We could not find a baseUrl in your gatsby-config.js',
+            new Error('Missing baseURL in config')
+        )
         return
     }
-    log(`Using: ${baseUrl}`)
+    reporter.verbose(`Using: ${baseUrl}`)
 
     if (!api.key || !api.secret) {
-        log(
-            chalk.bgRed(
-                'You seem to be missing Gravity Forms API details in your gatsby-config.js'
-            )
+        reporter.panic(
+            'You seem to be missing Gravity Forms API details in your gatsby-config.js',
+            new Error('Misisng api keys in config')
         )
         return
     }
@@ -63,9 +62,10 @@ exports.sourceNodes = async (
     // Check to make sure we got forms. If issues occured
     // need to stop here
     if (formsObj) {
-        log(chalk.black.bgWhite('Processing forms'))
+        const entriesArr = Object.entries(formsObj)
+        reporter.info(`Processing ${entriesArr.length} Gravity Forms`)
 
-        for (const [key, value] of Object.entries(formsObj)) {
+        for (const [key, value] of entriesArr) {
             createNode(
                 processForms(
                     createContentDigest,
@@ -76,6 +76,6 @@ exports.sourceNodes = async (
             )
         }
 
-        log(chalk.black.bgWhite('Completed Gravity Forms Source plugin'))
+        reporter.verbose('Completed Gravity Forms Source plugin')
     }
 }
